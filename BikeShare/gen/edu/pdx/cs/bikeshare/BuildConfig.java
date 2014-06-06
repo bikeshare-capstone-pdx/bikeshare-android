4dc025ff80596325fa4ca24af83024917269c26d
Colleen Murphy <cmurphy@cat.pdx.edu>
2014-06-06 14:33:00 -0700

    Initialize BikeShare Android app

---
+++ BikeShare/gen/edu/pdx/cs/bikeshare/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.pdx.cs.bikeshare;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Bikeshare Android App
+=====================
+
+Spring-Summer 2014 Portland State University Capstone Project (Team B)
