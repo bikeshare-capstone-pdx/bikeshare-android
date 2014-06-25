29a9386ba663dd043456bd26dd60c5fc7123937f
sarakane <kane.sarak@gmail.com>
2014-06-25 13:46:13 -0700

    Added launcher icon and map

--- BikeShare/src/edu/pdx/cs/bikeshare/MainActivity.java
+++ BikeShare/src/edu/pdx/cs/bikeshare/MainActivity.java
@@ -1,10 +1,17 @@
 package edu.pdx.cs.bikeshare;
 
+
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import edu.pdx.cs.bikeshare.R;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,18 +21,31 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
-
+	MyItemizedOverlay myItemizedOverlay = null;
+	 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+         
+        Drawable marker=getResources().getDrawable(android.R.drawable.star_big_on);
+        int markerWidth = marker.getIntrinsicWidth();
+        int markerHeight = marker.getIntrinsicHeight();
+        marker.setBounds(0, markerHeight, markerWidth, 0);
+         
+        ResourceProxy resourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
+         
+        myItemizedOverlay = new MyItemizedOverlay(marker, resourceProxy);
+        mapView.getOverlays().add(myItemizedOverlay);
+         
+        GeoPoint myPoint1 = new GeoPoint(0*1000000, 0*1000000);
+        myItemizedOverlay.addItem(myPoint1, "myPoint1", "myPoint1");
+        GeoPoint myPoint2 = new GeoPoint(50*1000000, 50*1000000);
+        myItemizedOverlay.addItem(myPoint2, "myPoint2", "myPoint2");
+         
+    } 
 
 
     @Override
@@ -50,8 +70,8 @@ public class MainActivity extends ActionBarActivity {
         case R.id.action_settings:
         	//openSettings();
         	return true;
-        case R.id.action_favorites:
-        	return true;
+        /*case R.id.action_favorites:
+        	return true;*/
         case R.id.action_login:
         	return true;
         }
@@ -61,17 +81,17 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+//    public static class PlaceholderFragment extends Fragment {
+//
+//        public PlaceholderFragment() {
+//        }
+//
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                Bundle savedInstanceState) {
+//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+//            return rootView;
+//        }
+//    }
 
 }
