69a9e52fd2922a3d921ebd7a349bc84d903af03f
Craig Meinschein <cmeinsch@gmail.com>
2014-08-10 10:28:01 -0700

    Corrected whitespace.
    Because eclipse-- likes to sometimes make arbitrary decisions about it.

--- BikeShare/src/edu/pdx/cs/bikeshare/MyItemizedOverlay.java
+++ BikeShare/src/edu/pdx/cs/bikeshare/MyItemizedOverlay.java
@@ -70,93 +70,93 @@ public class MyItemizedOverlay extends ItemizedOverlay<OverlayItem> {
 	@Override
 	protected boolean onTap(int index) {
 		OverlayItem item = overlayItemList.get(index);
-        Station s = (Station)item;
+		Station s = (Station)item;
 		new ShowStationInfo().execute(s.station_id);
-    return true;
+		return true;
 	}
 	
 	private class ShowStationInfo extends AsyncTask<Integer, Void, String> {
-        private final static String route = "/REST/1.0/stations/info/%d";
-        private final static String tag = "ShowAllStations";
-        
-        @Override
-        protected String doInBackground(Integer... params) {
-            // Call REST API to find BikeShare station info.
-            HttpClient web = new DefaultHttpClient();
-            String apiData = null;
-            try {
-                HttpResponse resp = web.execute(new HttpGet(apiUrl + String.format(route, params[0])));
-                StatusLine status = resp.getStatusLine();
-                if (status.getStatusCode() == HttpStatus.SC_OK) {
-                    // REST API returned success.
-                    ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    resp.getEntity().writeTo(out);
-                    out.close();
-                    apiData = out.toString();
-                } else {
-                    // REST API returned an error.
-                    resp.getEntity().getContent().close();
-                    Log.e(tag,"REST API returned error: " + status.getReasonPhrase());
-                }
-            } catch (ClientProtocolException e) {
-                // HTTP barfed.
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return apiData;
-        }
-        
-        @Override
-        protected void onPostExecute(String result) {
-            // If we received data from the API, parse the JSON.
-            if (result != null) {
-                try {
-                    JSONObject jStation = new JSONObject(result);
-                    // Construct a station object from the data.
-                    int station_id = jStation.getInt("STATION_ID");
-                    String station_name = jStation.getString("STATION_NAME");
-                    String street_address = jStation.getString("STREET_ADDRESS");
-                    int current_bikes = jStation.getInt("CURRENT_BIKES");
-                    int current_docks = jStation.getInt("CURRENT_DOCKS");
-                    int current_discount = jStation.getInt("CURRENT_DISCOUNT");
-                    String checkoutMsg = "Station ID: %d\n";
-                    checkoutMsg += "Address: %s\n";
-                    checkoutMsg += "Number of bikes available: %d\n";
-                    checkoutMsg += "Check out bike?";
-                    // Display dialog box asking if the user wants to check out a bike.
-                    AlertDialog.Builder checkOut = new AlertDialog.Builder(mContext);
-                    checkOut.setMessage(String.format(checkoutMsg, station_id, street_address, current_bikes)).setTitle(station_name)
-                    .setPositiveButton(R.string.check_out, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                        }
-                    })
-                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // User cancelled the dialog
-                        }
-                    });
-                    checkOut.show();
-                    // Update the data structure containing the map pins with the additional API info.
-                    for (OverlayItem item : overlayItemList) {
-                    	if (item instanceof Station) {
-                    		Station s = (Station)item;
-                    		if (s.station_id == station_id) {
-                    			s.current_bikes = current_bikes;
-                    			s.current_docks = current_docks;
-                    			s.current_discount = current_discount;
-                    			break;
-                    		}
-                    	}
-                    }
-                } catch (JSONException e) {
-                    // Failed to parse the JSON.
-                    e.printStackTrace();
-                }
-            } else {
-                // Failed to get data from the API.
-            }
-        }
+		private final static String route = "/REST/1.0/stations/info/%d";
+		private final static String tag = "ShowAllStations";
+
+		@Override
+		protected String doInBackground(Integer... params) {
+			// Call REST API to find BikeShare station info.
+			HttpClient web = new DefaultHttpClient();
+			String apiData = null;
+			try {
+				HttpResponse resp = web.execute(new HttpGet(apiUrl + String.format(route, params[0])));
+				StatusLine status = resp.getStatusLine();
+				if (status.getStatusCode() == HttpStatus.SC_OK) {
+					// REST API returned success.
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					resp.getEntity().writeTo(out);
+					out.close();
+					apiData = out.toString();
+				} else {
+					// REST API returned an error.
+					resp.getEntity().getContent().close();
+					Log.e(tag,"REST API returned error: " + status.getReasonPhrase());
+				}
+			} catch (ClientProtocolException e) {
+				// HTTP barfed.
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return apiData;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// If we received data from the API, parse the JSON.
+			if (result != null) {
+				try {
+					JSONObject jStation = new JSONObject(result);
+					// Construct a station object from the data.
+					int station_id = jStation.getInt("STATION_ID");
+					String station_name = jStation.getString("STATION_NAME");
+					String street_address = jStation.getString("STREET_ADDRESS");
+					int current_bikes = jStation.getInt("CURRENT_BIKES");
+					int current_docks = jStation.getInt("CURRENT_DOCKS");
+					int current_discount = jStation.getInt("CURRENT_DISCOUNT");
+					String checkoutMsg = "Station ID: %d\n";
+					checkoutMsg += "Address: %s\n";
+					checkoutMsg += "Number of bikes available: %d\n";
+					checkoutMsg += "Check out bike?";
+					// Display dialog box asking if the user wants to check out a bike.
+					AlertDialog.Builder checkOut = new AlertDialog.Builder(mContext);
+					checkOut.setMessage(String.format(checkoutMsg, station_id, street_address, current_bikes)).setTitle(station_name)
+					.setPositiveButton(R.string.check_out, new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+						}
+					})
+					.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							// User cancelled the dialog
+						}
+					});
+					checkOut.show();
+					// Update the data structure containing the map pins with the additional API info.
+					for (OverlayItem item : overlayItemList) {
+						if (item instanceof Station) {
+							Station s = (Station)item;
+							if (s.station_id == station_id) {
+								s.current_bikes = current_bikes;
+								s.current_docks = current_docks;
+								s.current_discount = current_discount;
+								break;
+							}
+						}
+					}
+				} catch (JSONException e) {
+					// Failed to parse the JSON.
+					e.printStackTrace();
+				}
+			} else {
+				// Failed to get data from the API.
+			}
+		}
 	}
 }
 
