441a3b688fd28a47a2a6cd157a741179c180ee28
sarakane <kane.sarak@gmail.com>
2014-08-18 07:35:47 -0700

    Added missing icons, removed "search" from the action bar.

--- BikeShare/src/edu/pdx/cs/bikeshare/MainActivity.java
+++ BikeShare/src/edu/pdx/cs/bikeshare/MainActivity.java
@@ -80,10 +80,6 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-    	switch (item.getItemId()) {
-        case R.id.action_search:
-        	return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
